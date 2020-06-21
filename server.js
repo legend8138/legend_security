2c95e32a849a1b66ee58e6730fbf5482747507a9
Glitch (agrinmwe) <none>
2020-06-21 17:31:59 +0000

    🙅📖 Checkpoint ./antibot.json:16842264/10 ./server.js:16842264/118

--- server.js
+++ server.js
@@ -747,5 +747,5 @@ bot.on("message", message => {
 });
 
 
-bot.login("NzA2MzA0MDg0MDEzMDg4Nzgw.Xu43qQ.YAP38B37S0eirtv5B52iwsyTebI");
+bot.login("NzA2MzA0MDg0MDEzMDg4Nzgw.Xu9FuQ.PF4nrwjs_e7tpqs1cg6BXjquKjs");
 
