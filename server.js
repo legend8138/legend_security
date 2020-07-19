614ca42ae1028f1d23f487b26d6430b108aafc73
Glitch (biomine) <none>
2020-07-19 10:20:52 +0000

    👾🙉 Checkpoint ./server.js:18067396/6 ./antibot.json:18067396/2211

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ console.log(
 let statuses = [
 ///// لێرانە شتێک بووسە بۆ ستریمینگەکە
 //// DASTKARE MAKA DACHE BGYE BEKAYTA EROR BA WRYAYI DASKARII MAKA
-`Servers: 133 | Users: 54968`,
+`Servers: 158 | Users: 94968`,
 `m!help It's time to secure your server!`,
 ];
 setInterval(function() {
